a1888525222682233fbce993aeb50219830ba324
karanchauke <karanchauke7083@outlook.com>
2024-06-30 14:15:28 +0530

    Postgres Database Testing Here!

---
+++ src/create-user.ts
@@ -0,0 +1,25 @@
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+        email : "karanshrma46@gmail.com" ,
+        name : "Karan C"
+    }
+  })
+}
+
+main()
+    .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+  
